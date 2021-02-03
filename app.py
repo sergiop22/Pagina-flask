98e62d36b021d3640d1848d6683de85c5a241c75
sergiop22 <sergiopadillan97@gmail.com>
2021-02-03 14:46:43 -0600

    Translate

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, session
 from pytube import YouTube
 import time
+from googletrans import Translator
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sergiosergio'
@@ -32,8 +33,82 @@ def downloader():
 def encrypt():
     return render_template("encrypt.html")
 
-@app.route('/translate')
+@app.route('/translate', methods=['GET', 'POST'])
 def translate():
+    session['choose_language']= [
+                        'Afrikaans',
+                        'Albanian',
+                        'Arabic',
+                        'Armenian',
+                        'Belarusian',
+                        'Bengali',
+                        'Bosnian',
+                        'Bulgarian',
+                       ' Catalan',
+                        'Chinese',
+                        'Croatian',
+                       ' Czech',
+                        'Danish',
+                        'Dutch',
+                        'English',
+                        'Esperanto',
+                        'Estonian',
+                        'Filipino',
+                        'Finnish',
+                        'French',
+                        'Galician',
+                        'German',
+                        'Greek',
+                        'Hawaiian',
+                        'Hebrew',
+                        'Hindi',
+                        'Hungarian',
+                        'Icelandic',
+                        'Indonesian',
+                        'Irish',
+                        'Italian',
+                        'Japanese',
+                        'Korean',
+                        'Lao',
+                        'Latin',
+                        'Lithuanian',
+                        'Luxembourgish',
+                        'Macedonian',
+                        'Maltese',
+                        'Maori',
+                        'Mongolian',
+                        'Nepali',
+                        'Norwegian'
+                        'Persian',
+                        'Polish',
+                        'Portuguese',
+                        'Romanian',
+                        'Russian',
+                        'Serbian',
+                        'Slovenian',
+                        'Somali',
+                        'Spanish',
+                        'Swedish',
+                        'Tajik',
+                        'Tamil',
+                        'Thai',
+                        'Turkish',
+                        'Ukrainian',
+                        'Vietnamese',
+                        'Zulu']
+
+    if request.method == "POST":
+        lan = request.form.get('drop')
+        text = request.form['trans']
+
+        if text == '':
+                flash('please fill the box')
+        else:
+                translator = Translator()
+                output = translator.translate(text, dest=lan)
+                output = output.text
+                return render_template("translate.html", **locals()) 
+
     return render_template("translate.html") 
 
 if __name__ == '__main__':
